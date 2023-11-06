46cf1f5a908f10ad176579041a54dc2496a02d7c
AntiKiljoy <hstconnor@gmail.com>
2023-11-06 15:17:29 +0100

    Nearly all find buttons work

--- events.html/js/mappage.js
+++ events.html/js/mappage.js
@@ -1,6 +1,6 @@
 // THIS IS FOR THE MUSIC ROOM
 document.querySelector(".findMusic").addEventListener("click", function () {
-    const pathToHighlight = document.getElementById("zone34"); // zone34 is the section to highlight, that I have gave an ID to.
+    const pathToHighlight = document.getElementById("zone40b"); // zone40b is the section to highlight, that I have gave an ID to.
   
     if (pathToHighlight) {
       if (pathToHighlight.classList.contains("highlighted")) {
@@ -10,3 +10,68 @@ document.querySelector(".findMusic").addEventListener("click", function () {
       }
     }
   });
+
+
+// THIS IS FOR THE SAUNA
+document.querySelector(".findSauna").addEventListener("click", function () {
+  const pathToHighlight = document.getElementById("zone40b"); 
+
+  if (pathToHighlight) {
+    if (pathToHighlight.classList.contains("highlighted")) {
+      pathToHighlight.classList.remove("highlighted");
+    } else {
+      pathToHighlight.classList.add("highlighted");
+    }
+  }
+});
+
+// THIS IS FOR THE CARETAKER
+document.querySelector(".findCaretaker").addEventListener("click", function () {
+  const pathToHighlight = document.getElementById("zone46"); 
+
+  if (pathToHighlight) {
+    if (pathToHighlight.classList.contains("highlighted")) {
+      pathToHighlight.classList.remove("highlighted");
+    } else {
+      pathToHighlight.classList.add("highlighted");
+    }
+  }
+});
+
+// THIS IS FOR THE BAR
+document.querySelector(".findBar").addEventListener("click", function () {
+  const pathToHighlight = document.getElementById("zone40b"); 
+
+  if (pathToHighlight) {
+    if (pathToHighlight.classList.contains("highlighted")) {
+      pathToHighlight.classList.remove("highlighted");
+    } else {
+      pathToHighlight.classList.add("highlighted");
+    }
+  }
+});
+
+// THIS IS FOR THE LAUNDRY
+document.querySelector(".findLaundry").addEventListener("click", function () {
+  const pathToHighlight = document.getElementById("zone36"); 
+
+  if (pathToHighlight) {
+    if (pathToHighlight.classList.contains("highlighted")) {
+      pathToHighlight.classList.remove("highlighted");
+    } else {
+      pathToHighlight.classList.add("highlighted");
+    }
+  }
+});
+
+// THIS IS FOR THE GAMES ROOM
+document.querySelector(".findGames").addEventListener("click", function () {
+  const pathToHighlight = document.getElementById("zone42"); 
+  if (pathToHighlight) {
+    if (pathToHighlight.classList.contains("highlighted")) {
+      pathToHighlight.classList.remove("highlighted");
+    } else {
+      pathToHighlight.classList.add("highlighted");
+    }
+  }
+});
