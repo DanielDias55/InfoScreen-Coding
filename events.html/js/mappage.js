63ed46c35ea9866654c5f35dbac38d850fa6c929
AntiKiljoy <hstconnor@gmail.com>
2023-11-07 11:08:19 +0100

    Big changes
    Javascript changes. So, instead of many instances of 'event listerns', it is now more simplified

--- events.html/js/mappage.js
+++ events.html/js/mappage.js
@@ -1,77 +1,17 @@
-// THIS IS FOR THE MUSIC ROOM
-document.querySelector(".findMusic").addEventListener("click", function () {
-    const pathToHighlight = document.getElementById("zone40b"); // zone40b is the section to highlight, that I have gave an ID to.
-  
+const buttons = document.querySelectorAll('.find');
+
+buttons.forEach(button => {
+  button.addEventListener('click', function () {
+    const zoneId = button.getAttribute('data-zone');
+    const pathToHighlight = document.getElementById(zoneId);
+
     if (pathToHighlight) {
-      if (pathToHighlight.classList.contains("highlighted")) {
+      pathToHighlight.classList.add("highlighted");
+
+      // Remove the 'highlighted' class after 3 seconds (3000 milliseconds)
+      setTimeout(function () {
         pathToHighlight.classList.remove("highlighted");
-      } else {
-        pathToHighlight.classList.add("highlighted");
-      }
+      }, 3000);
     }
   });
-
-
-// THIS IS FOR THE SAUNA
-document.querySelector(".findSauna").addEventListener("click", function () {
-  const pathToHighlight = document.getElementById("zone40b"); 
-
-  if (pathToHighlight) {
-    if (pathToHighlight.classList.contains("highlighted")) {
-      pathToHighlight.classList.remove("highlighted");
-    } else {
-      pathToHighlight.classList.add("highlighted");
-    }
-  }
-});
-
-// THIS IS FOR THE CARETAKER
-document.querySelector(".findCaretaker").addEventListener("click", function () {
-  const pathToHighlight = document.getElementById("zone46"); 
-
-  if (pathToHighlight) {
-    if (pathToHighlight.classList.contains("highlighted")) {
-      pathToHighlight.classList.remove("highlighted");
-    } else {
-      pathToHighlight.classList.add("highlighted");
-    }
-  }
-});
-
-// THIS IS FOR THE BAR
-document.querySelector(".findBar").addEventListener("click", function () {
-  const pathToHighlight = document.getElementById("zone40b"); 
-
-  if (pathToHighlight) {
-    if (pathToHighlight.classList.contains("highlighted")) {
-      pathToHighlight.classList.remove("highlighted");
-    } else {
-      pathToHighlight.classList.add("highlighted");
-    }
-  }
-});
-
-// THIS IS FOR THE LAUNDRY
-document.querySelector(".findLaundry").addEventListener("click", function () {
-  const pathToHighlight = document.getElementById("zone36"); 
-
-  if (pathToHighlight) {
-    if (pathToHighlight.classList.contains("highlighted")) {
-      pathToHighlight.classList.remove("highlighted");
-    } else {
-      pathToHighlight.classList.add("highlighted");
-    }
-  }
-});
-
-// THIS IS FOR THE GAMES ROOM
-document.querySelector(".findGames").addEventListener("click", function () {
-  const pathToHighlight = document.getElementById("zone42"); 
-  if (pathToHighlight) {
-    if (pathToHighlight.classList.contains("highlighted")) {
-      pathToHighlight.classList.remove("highlighted");
-    } else {
-      pathToHighlight.classList.add("highlighted");
-    }
-  }
 });
