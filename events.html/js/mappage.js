bf650aafe0ed1f953973dd7491c7945fa27afd6a
AntiKiljoy <hstconnor@gmail.com>
2023-11-07 11:08:42 +0100

    Update mappage.js

--- events.html/js/mappage.js
+++ events.html/js/mappage.js
@@ -2,13 +2,13 @@ const buttons = document.querySelectorAll('.find');
 
 buttons.forEach(button => {
   button.addEventListener('click', function () {
-    const zoneId = button.getAttribute('data-zone');
+    const zoneId = button.getAttribute('data-zone'); // HTML data-* Attributes.
     const pathToHighlight = document.getElementById(zoneId);
 
     if (pathToHighlight) {
       pathToHighlight.classList.add("highlighted");
 
-      // Remove the 'highlighted' class after 3 seconds (3000 milliseconds)
+      // Remove the highlighted  after 3 seconds (3000 milliseconds)
       setTimeout(function () {
         pathToHighlight.classList.remove("highlighted");
       }, 3000);
