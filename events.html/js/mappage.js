bb6bef0695c8db5f13efc758a7cefed83658a6ff
AntiKiljoy <hstconnor@gmail.com>
2023-11-08 08:49:42 +0100

    Updates.

--- events.html/js/mappage.js
+++ events.html/js/mappage.js
@@ -15,3 +15,26 @@ buttons.forEach(button => {
     }
   });
 });
+
+document.getElementById("openPopup").addEventListener("click", function() {
+  document.getElementById("bookingPopup").style.display = "block";
+  document.querySelector(".overlay").style.display = "block";
+});
+
+document.getElementById("closePopup").addEventListener("click", function() {
+  document.getElementById("bookingPopup").style.display = "none";
+  document.querySelector(".overlay").style.display = "none";
+});
+
+document.getElementById("bookingForm").addEventListener("submit", function(event) {
+  event.preventDefault();
+  // Handle form submission here, e.g., send data to a server
+  // You can also close the popup after successful submission
+  document.getElementById("bookingPopup").style.display = "none";
+  document.querySelector(".overlay").style.display = "none";
+});
+
+document.querySelector(".overlay").addEventListener("click", function() {
+  document.getElementById("bookingPopup").style.display = "none";
+  this.style.display = "none";
+});
